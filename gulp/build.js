71db35dc15a7b5abd4e0a9f24a466cd590c40204
Matias Aiskovich <matias.aiskovich@caretronics.com.ar>
2016-04-01 09:42:17 -0300

    Adds font awesome gulp task, changes in date time picker.

--- gulp/build.js
+++ gulp/build.js
@@ -50,6 +50,7 @@ gulp.task('html', ['inject', 'partials'], function () {
     .pipe(cssFilter)
     .pipe($.sourcemaps.init())
     .pipe($.replace('../../bower_components/material-design-iconfont/iconfont/', '../fonts/'))
+    .pipe($.replace('../../bower_components/font-awesome/fonts/', '../fonts/'))
     .pipe($.minifyCss({ processImport: false }))
     .pipe($.sourcemaps.write('maps'))
     .pipe(cssFilter.restore)
@@ -71,6 +72,13 @@ gulp.task('html', ['inject', 'partials'], function () {
 // Only applies for fonts from bower dependencies
 // Custom fonts are handled by the "other" task
 gulp.task('fonts', function () {
+  return gulp.src($.mainBowerFiles().concat('bower_components/font-awesome/fonts/*'))
+    .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2}'))
+    .pipe($.flatten())
+    .pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/')));
+});
+//Fonts awesome copying files
+gulp.task('fonts-awesome', function () {
   return gulp.src($.mainBowerFiles().concat('bower_components/material-design-iconfont/iconfont/*'))
     .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2}'))
     .pipe($.flatten())
@@ -104,4 +112,4 @@ gulp.task('clean', function () {
   return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
 });
 
-gulp.task('build', ['html', 'fonts', 'other', 'sw']);
+gulp.task('build', ['html', 'fonts', 'fonts-awesome','other', 'sw']);
